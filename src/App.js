7dfa24bbd2b945d569ef3bc33816aff9f859b8bf
nitigya gurung <grgniti07@gmail.com>
2022-03-21 16:31:57 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css';
 
-function App() {
+const App =() => {
+  const [diceNumber, setDiceNumber] = useState(4)
+  const refreshDice=()=>{
+      const ranno = Math.floor(Math.random() * 6) + 1
+      setDiceNumber(ranno)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+      <img width={300} height={300} src={require(`./assets/${diceNumber}.png`)}></img>
+      <br/>
+      <button onClick={()=> refreshDice()} className='button'>Roll</button>
+      </center>
     </div>
   );
 }
